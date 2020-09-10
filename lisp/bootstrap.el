f1c89e3fe97be3b10d6ec33cdb362d521178a990
Dustin Lacewell <dlacewell@gmail.com>
2020-09-10 08:34:31 -0500

    Isolate data directory to ~/.config/babix/

--- lisp/bootstrap.el
+++ lisp/bootstrap.el
@@ -1,3 +1,8 @@
+(setq babix-home (concat (getenv "HOME") "/.config/" "babix/"))
+(setq user-emacs-directory babix-home)
+(unless (file-exists-p babix-home)
+  (make-directory babix-home))
+
 (let ((lisp-path (file-name-directory load-file-name)))
   (add-to-list 'load-path lisp-path)
   (add-to-list 'load-path (concat lisp-path "ob"))
